39fb3729a1093d10f29cbd16be72dd7a7f88fea9
Chesterkxng <cgoita00@gmail.com>
2023-07-16 19:11:36 -0700

    faut pas me fatiguer hein

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,32 +11,30 @@
  */
 int main(void)
 {
-int i, j, k, l;
-i = 0;
-j = 0;
-k = 0;
-l = 0;
-for (; i < 10; i++)
-{
-k = i;
-for (; j < 10; j++)
-{
-l = j + 1;
-for (; k < 10; k++)
-{
-for (; l < 10; l++)
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i = 0, j = 0, k = 0, l = 0;
+
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			for (; k < 10; k++)
+			{
+				for (; l < 10; l++)
+				{
+					if (i * 10 + j < k * 10 + l)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
