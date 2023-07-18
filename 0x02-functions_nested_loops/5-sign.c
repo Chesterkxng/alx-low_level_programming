1bacf6ff759e2c077d2034d4aef2683866ea7401
Chesterkxng <cgoita00@gmail.com>
2023-07-18 09:30:47 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Description: a program that prints the sign
+ * of a number
+ *
+ * @n: character(int) to be checked
+ *
+ * Return: 0 (if is zero)
+ *         1 (if is greater than zero)
+ *        -1 (if is less than zero)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+
+}
