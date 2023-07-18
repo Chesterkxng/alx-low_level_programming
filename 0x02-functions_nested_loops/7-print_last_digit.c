6fbc31d5e8f574f4ca40249475f59c9df0c53249
Chesterkxng <cgoita00@gmail.com>
2023-07-18 11:23:10 -0700

    last digit of

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,16 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
 	if (n >= 0)
 	{
-		return (n / 10);
+		last_digit = n % 10;
 	}
 	else
 	{
-		return ((-1 * n) / 10);
+		last_digit = (-1 * n) % 10;
 	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 
 }
