4d1015630cd738b5eb8e333a7b129c969638b8bf
Chesterkxng <cgoita00@gmail.com>
2023-07-18 10:38:14 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description:  return the absolute value of a given int 
+ *
+ * @c: character(int) to be checked
+ *
+ * Return: 0 (is lowercase)
+ *         1 (otherwise)
+ */
+
+int _abs(int)
+{
+	if (int >= 0)
+	{
+		return (int);
+	}
+	else
+	{
+		return (-1 * int);
+	}
+
+}
