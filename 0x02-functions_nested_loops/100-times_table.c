a72e9c431900387730429c9053086ba4cdccd4c0
Chesterkxng <cgoita00@gmail.com>
2023-07-19 10:54:33 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+int number_of_digits(int n);
+
 /**
  * print_times_table - Write a function that prints times table from 0 to 15.
  * @n: times table integer limit
@@ -9,22 +11,30 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
 
 		for (i = 0; i <= n; i++)
 		{
+			printf("0,");
 			for (j = 0; j <= n; j++)
 			{
 				int res;
 
 				res = i * j;
 
+				digits = number_of_digits(res);
+
+				for (k = 0; k <= (4 - digits); k++)
+				{
+					printf(" ");
+				}
+
 				if (j != n)
 				{
-					printf("%d,  ", res);
+					printf("%d,", res);
 				}
 				else
 				{
@@ -38,4 +48,24 @@ void print_times_table(int n)
 	{
 		printf("\n");
 	}
+}
+
+/**
+ *
+ * number_of_digits - count the number of digit of an integer
+ * @n: input integer
+ * Return: the number of digit of the input
+ */
+
+int number_of_digits(int n)
+{
+	int count;
+	count = 0;
+	do
+	{
+		n / 10;
+		count++;
+	}
+	while (n != 0);
+	return (count);
 }
