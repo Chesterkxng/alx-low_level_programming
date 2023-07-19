fec3c5cd1ec4f7547611f057aafd632b96ba80ef
Chesterkxng <cgoita00@gmail.com>
2023-07-19 11:15:09 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	if (n = 0 && n <= 15)
 	{
 
 		for (i = 0; i <= n; i++)
@@ -43,9 +43,9 @@ void print_times_table(int n)
 			}
 		}
 	}
-	else
+	else if (n == 0)
 	{
-		printf("\n");
+		printf("0");
 	}
 }
 
