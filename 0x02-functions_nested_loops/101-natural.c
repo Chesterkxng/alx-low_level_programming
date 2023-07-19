2807cb63a8387cc59565e8ebc40af1a9ecb30d5c
Chesterkxng <cgoita00@gmail.com>
2023-07-19 15:04:30 -0700

    modulo 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -16,10 +16,10 @@ int print_sum(int n)
 
 	sum = 0;
 
-	for (i = 1; i < n < 1024; i++)
+	for (i = 1; i < n; i++)
 	{
 		j = is_divide(i);
-		if (j == 1)
+		if (j == 1 && i < 1024)
 		{
 			sum = sum + j;
 		}
