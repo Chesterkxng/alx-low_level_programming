284271257edf64c074527a7d82daebd578d847c7
Chesterkxng <cgoita00@gmail.com>
2023-07-18 17:49:15 -0700

    road to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			print_digits(i);
-			if (n != 98)
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -30,7 +30,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			print_digits(i);
-			if (n != 98)
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
