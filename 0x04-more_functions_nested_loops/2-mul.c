45b97501e3ad2e2f84c4caa036dae2d5960ac67e
Chesterkxng <cgoita00@gmail.com>
2023-07-20 06:50:59 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Entry point
+ * Description:  a program that statuate if the character is uppercase or no
+ * @a : first input integer
+ * @b : second input integer
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
