ef5e37967414e963b6b5e6b7df6b18b5f53d8e8b
Chesterkxng <cgoita00@gmail.com>
2023-07-20 07:01:55 -0700

    from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Entry point
+ * Description:  a program that print all the numbers
+ * from 0 to 9 followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
