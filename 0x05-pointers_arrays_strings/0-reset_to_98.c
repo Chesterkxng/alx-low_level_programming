ff3b009e0487950a619d18950d493488b624c6dd
Chesterkxng <cgoita00@gmail.com>
2023-07-24 11:24:55 -0700

    reset to 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ * Description:  a program that take a pointer
+ * to an int as parameter and update the value
+ *
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This repository is used for pushing files necessary to validate the courses:
 - 0x02. C - Functions nested loops
 - 0x03. C - Debugging
 - 0x04. C - More function nested loops
+- 0X05. C - Pointers, array and strings
