17c5beb7e1329892ceb6a11b8635671a5828d89a
Chesterkxng <cgoita00@gmail.com>
2023-07-24 12:14:55 -0700

    number of characters

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ * Description:  a program that return the length of a given
+ * char
+ * @s: given char
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (s[count] != "")
+	{
+		count++;
+	}
+	return (count);
+}
