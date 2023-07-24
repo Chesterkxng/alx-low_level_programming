da5acc2122d2e8905179f4a4bb0e85597ca05d21
Chesterkxng <cgoita00@gmail.com>
2023-07-24 15:35:14 -0700

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * print_rev - Entry point
+ * Description:  a program that reverse print a
+ * string
+ * @s: given string
+ * Return: the reverse sting
+ */
+void print_rev(char *s)
+{
+	int length;
+
+	length = _strlen(s);
+
+	while (s[length] != '\0')
+	{
+		_putchar(str[length]);
+		length--;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - Entry point
+ * Description:  a program that return the length of a given
+ * char
+ * @s: given char
+ * Return: the number of characters
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
