7ebfae481ddfc8b39e36567c317009bc760a68a0
Chesterkxng <cgoita00@gmail.com>
2023-07-24 15:05:54 -0700

    like puts with stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - a function that print a string.
+ * @str : String
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(i + '0');
+		i++;
+	}
+}
