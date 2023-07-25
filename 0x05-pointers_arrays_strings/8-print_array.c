52a6d47e55065f83ba641f387bccb46a8f2e4082
Chesterkxng <cgoita00@gmail.com>
2023-07-24 18:20:09 -0700

    arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * print_array - Entry point
+ * Description:  a program that reverse print a
+ * string
+ * @a: given array
+ * @n: given int
+ * Return: the n value of the array separate by a comma
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= n)
+	{
+		if (i != n)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
