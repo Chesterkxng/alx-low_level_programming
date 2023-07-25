04e9c157418c6ba52ec7c878da9864a8b47450a4
Chesterkxng <cgoita00@gmail.com>
2023-07-24 18:04:57 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 {
 	int i, length;
 
-	length = _strlen(str) - 1;
+	length = _strlen(str);
 
 	if (length % 2 == 0)
 	{
@@ -21,7 +21,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		i = (length - 1) / 2;
+		i = (length / 2) + 1;
 	}
 	while (i < length)
 	{
