5e95fc239746d313e908188e66412bfb1a395cf6
Chesterkxng <cgoita00@gmail.com>
2023-07-26 12:51:11 -0700

    comparate 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * *_strcmp - Entry point.
+ * Description: a function compare two strings
+ * @s1 : first char
+ * @s2 : second char
+ *
+ * Return: the desired output.
+ */
+int *_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+}
+/**
+ * _strlen - Entry point
+ * Description:  a program that return the length of a given
+ * char
+ * @s: given char
+ * Return: the number of characters
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
