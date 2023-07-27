d603629dafd99d7f97328ee09afc13d6ca6bd463
Chesterkxng <cgoita00@gmail.com>
2023-07-27 06:11:16 -0700

    encode strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,8 +14,11 @@ char *leet(char *s)
 {
 	int i, j, n;
 
-	char spe_char[5][3] = {{'a', 'A', '4'}, {'e', 'E', '3'}, {'o', 'O', '0'},
-			   {'t', 'T', '7'}, {'l', 'L', '1'}};
+	char spe_char[10][2] = {{'a', '4'}, {'A', '4'},
+			       {'e', '3'}, {'E', '4'},
+			       {'o', '0'}, {'O', '0'},
+			       {'t', '7'}, {'T', '7'},
+			       {'l', '1'}, {'L', '1'}};
 
 	i = 0;
 
@@ -27,11 +30,11 @@ char *leet(char *s)
 	{
 		j = 0;
 
-		while (j < 6)
+		while (j < 11)
 		{
-			if (s[i] == spe_char[j][0] || s[i] == spe_char[j][1])
+			if (s[i] == spe_char[j][0])
 			{
-				s[i] = spe_char[j][2];
+				s[i] = spe_char[j][1];
 			}
 			j++;
 		}
