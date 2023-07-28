998cb3b55716d88232c055d905e6939867772326
Chesterkxng <cgoita00@gmail.com>
2023-07-28 16:07:40 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,19 @@
  */
 void print_number(int n)
 {
-	if (sizeof(n) == sizeof(int))
-	{
-		if (n < 0)
+
+	if (n < 0)
+       	{
+	      	_putchar(45);
+		if (-n / 10 > 0)
 		{
-			n = -(n);
-			_putchar(45);
+			print_number(-n / 10);
 		}
-		if ((n / 10) > 0)
+		_putchar(-(n % 10) + 48);
+       	}
+	else
+	{
+		if (n / 10 > 0)
 		{
 			print_number(n / 10);
 		}
