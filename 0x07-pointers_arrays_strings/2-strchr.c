dc7993a86a2a23b9e6c0924a4df035bb8befd2c5
Chesterkxng <cgoita00@gmail.com>
2023-07-31 08:32:46 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * *_strchr - Entry point
+ * Description: function copies n bytes from memory area
+ * src to memory area dest
+ * @s: a pointer to a string
+ * @c: searched char
+ *
+ * Return: a pointer to a string.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
