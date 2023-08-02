a328f0e83672d8e24df766d99c3dce901b0a3da2
Chesterkxng <cgoita00@gmail.com>
2023-08-02 15:46:36 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+int guesser(int guess, int n);
+/**
+ * _sqrt_recursion - return the root square
+ * @n: a input (integer)
+ * Return: the root square of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int guess;
+
+	guess = 1;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (guesser(guess, n));
+	}
+
+}
+/**
+ * guesser - guesser of square root
+ * @guess: a input (integer)
+ * @n: a input (integer)
+ * Return: the guess
+ */
+
+int guesser(int guess, int n)
+{
+	if (guess * guess - n == 0)
+	{
+		return guess;
+	}
+	else if (guess != 1 && guess * guess - n < 0)
+	{
+		return (-1);
+	}
+	return (guesser((n / guess + guess) / 2, n));
+}
