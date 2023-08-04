46d97492165cc422b7afc025c72de0c7fa826765
Chesterkxng <cgoita00@gmail.com>
2023-08-04 11:51:15 -0700

    fixing check 16

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,42 @@
 #include "main.h"
 
-int _atoi(char *s);
+/**
+ * _atoi - Write a function that convert a string to an integer.
+ * @s : pointer to a char
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	int i, k, num, j;
+
+	i = 0;
+	k = 0;
+	num = 0;
+	j = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == '-' && j == 0)
+		{
+			k++;
+		}
+		if (*(s + i) >= 48 &&  *(s + i) <= 57)
+		{
+			if (k % 2 == 0)
+			{
+				num = num * 10 + (*(s + i) - 48);
+			}
+			else
+			{
+				num = num * 10 - (*(s + i) - 48);
+			}
+			j = i;
+		}
+		if (j != 0 && j != i)
+		{
+			break;
+		}
+		i++;
+	}
+	return (num);
+}
