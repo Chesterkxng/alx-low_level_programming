7d72688292492e745db91dbfaf1e3f77f1830cd1
Chesterkxng <cgoita00@gmail.com>
2023-08-04 19:05:42 -0700

    a program that says his name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a function that print the name of
+ * of the progran even after renaming
+ * @argc: number of arguments
+ * @argv: pointer to arguments
+ * Return: 0 succes
+ */
+int main(int argc,char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
