09235b025e625be1540c9835eccd16b35accffdf
Chesterkxng <cgoita00@gmail.com>
2023-08-08 13:47:31 -0700

    griding

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
 /**
  * **alloc_grid - Entry point
- * Description: Write a function that return a pointer to
- * the copy of the given char.
+ * Description: Write a function display a grid.
  * @width: input
  * @height: input
  * Return: null or pointer to array
@@ -17,41 +14,25 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-
-	}
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
-			{
 				free(grid[j]);
-			}
 			free(grid);
 			return (NULL);
 		}
 	}
-	i = 0;
-
-	while (i < height)
+	for (i = 0; i < height, i++)
 	{
-		j = 0;
-
-		while (j < width)
-		{
+		for (j = 0, i < width, j++)
 			grid[i][j] = 0;
-			j++;
-		}
-		i++;
 	}
 	return (grid);
 }
