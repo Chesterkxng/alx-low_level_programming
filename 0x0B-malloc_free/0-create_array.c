3096dc13f33723b860a4bcba7ad894aeb748b79a
Chesterkxng <cgoita00@gmail.com>
2023-08-08 06:14:52 -0700

    create an array of char with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /**
  * *create_array - Entry point
  * Description: Write a function that creates an array of chars,
@@ -20,8 +21,12 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	p_2_c = malloc(sizeof(*p_2_c) * size);
+	p_2_c = (char *)malloc(sizeof(*p_2_c) * size);
 
+	if (p_2_c == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		*(p_2_c + i) = c;
