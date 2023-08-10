6f799025230ff6bbd41aaa7131f7e446308137a6
Chesterkxng <cgoita00@gmail.com>
2023-08-10 11:30:35 -0700

    memset and malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
 /**
  * *_calloc - Entry point
  * Description: a function allocates memory for an array of nmemb
@@ -18,5 +18,29 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
+	_memset(p, 0, nmemb * size);
 	return (p);
+}
+/**
+ * *_memset - Entry point
+ * Description: function fills the first n bytes of the memory area pointed to
+ * by s with the constant byte b
+ * @s : a point to a string
+ * @b : substitution string
+ * @n : integer
+ *
+ * Return: a pointer to a string.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
