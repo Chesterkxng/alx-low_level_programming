e5220c93b3301f5597088c712722568993e14503
Chesterkxng <cgoita00@gmail.com>
2023-08-10 11:14:39 -0700

    allocation of memory (array)

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+int _strlen(char *s);
+/**
+ * *_calloc - Entry point
+ * Description: a function allocates memory for an array of nmemb
+ * elements of size.
+ * @nmemb: int
+ * @size: int
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
