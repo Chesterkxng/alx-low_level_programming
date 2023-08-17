a1258ff80af4f76a7bd586c1230233b3b958f71d
Chesterkxng <cgoita00@gmail.com>
2023-08-17 09:08:08 -0700

    trying with the end

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -46,11 +46,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		if (-n / 10 > 0)
+		if ((-1 * n) / 10 > 0)
 		{
 			print_number(-n / 10);
 		}
-		_putchar(-(n % 10) + 48);
+		_putchar(((-1 * n) % 10) + 48);
 	}
 	else
 	{
