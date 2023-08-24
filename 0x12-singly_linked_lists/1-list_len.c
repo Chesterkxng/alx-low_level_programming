2cbbcd89f2aefd61f77505c055f79d74fe918f22
Chesterkxng <cgoita00@gmail.com>
2023-08-24 06:42:48 -0700

    len of the linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,21 +4,17 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * print_list - a function that prints
- * all the elements of a list_t list
+ * list_len - a function that return
+ * the number of elements of a list_t list
  * @h: pointer
  * Return: size_t
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	unsigned long size = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
 		size++;
 		h = h->next;
 	}
