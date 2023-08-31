f605da6e71e39056df39d26eb9ba1d3233b1d1c2
Chesterkxng <cgoita00@gmail.com>
2023-08-31 11:08:16 -0700

    edge case for index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask;
 	int bit = get_bit(*n, index);
 
+	if (index > 64)
+		return (-1); 
 	if (bit == 1)
 	{
 		mask = 1 << index;
