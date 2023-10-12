0f17cd0523183b1ae922af3fdeac47b0717acabe
Chesterkxng <cgoita00@gmail.com>
2023-10-12 21:31:24 +0100

    edge case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->next = next;
 			return (new_node);
 		}
-		else if (i == idx - 1 && !next)
+		else if (!next)
 			return (add_dnodeint_end(h, n));
 		temp = temp->next;
 		i++;
