2c5dd9726cfbd749e4843ca5db347d246acadcd1
Chesterkxng <cgoita00@gmail.com>
2023-10-16 16:43:19 +0100

    op library

---
+++ 0x18-dynamic_libraries/op.c
@@ -0,0 +1,51 @@
+#include "op.h"
+/**
+ * add - addition
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mul
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divide
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return ((int) a / b);
+}
+/**
+ * mod - addition
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
