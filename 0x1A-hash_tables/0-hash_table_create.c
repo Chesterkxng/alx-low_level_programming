666941b61841de5c8d2b5ec0d75c6181a5edc738
Chesterkxng <cgoita00@gmail.com>
2023-11-02 18:21:45 +0000

    create ht

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_ht->size =  size;
 	new_ht->array = malloc(sizeof(hash_node_t) * size);
 	if (!new_ht->array)
+	{
 		free(new_ht);
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		new_ht->array[i] = NULL;
 	return (new_ht);
