e30a4c0fd7c54dc07ae1b36db3da8b98b51870e1
Chesterkxng <cgoita00@gmail.com>
2023-12-20 19:47:36 +0000

    Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ function that computer perimeter of island
+"""
+def island_perimeter(grid):
+        m = len(grid)
+        n = len(grid[0])
+        perimeter = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                        perimeter += 4
+                        if i > 0 and grid[i - 1][j] == 1:
+                                perimeter -= 2
+                        if j > 0 and grid[i][j - 1] == 1:
+                                perimeter -= 2
+        return perimeter
