c6d900fa084e68cb731e938c2754ff30673e48c0
Chesterkxng <cgoita00@gmail.com>
2023-12-20 20:04:23 +0000

    pystyle fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,17 +2,19 @@
 """
  function that computer perimeter of island
 """
+
+
 def island_perimeter(grid):
-        """ return the perimeter of island """
-        m = len(grid)
-        n = len(grid[0])
-        perimeter = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                        perimeter += 4
-                        if i > 0 and grid[i - 1][j] == 1:
-                                perimeter -= 2
-                        if j > 0 and grid[i][j - 1] == 1:
-                                perimeter -= 2
-        return perimeter
+    """ return the perimeter of island """
+    m = len(grid)
+    n = len(grid[0])
+    perimeter = 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
