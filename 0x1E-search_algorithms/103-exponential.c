6285fa82233499f60ebb6b237e421311acc65d4b
Chesterkxng <cgoita00@gmail.com>
2024-02-06 22:29:42 +0000

    case first element

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -51,7 +51,10 @@ int exponential_search(int *array, size_t size, int value)
 	if (!array || size == 0)
 		return (-1);
 	if (value == array[0])
+	{
+		printf("Value checked array[0] = [%d]\n", array[0]);
 		return (0);
+	}
 	do {
 		printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
 		temp_idx = idx;
